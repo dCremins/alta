26c1deb47e1f8edb91fcd2651d2cc018833f1202
Devin Cremins <devin@inspectionxpert.com>
2021-11-07 15:03:52 -0500

    configs

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 200,
+  singleQuote: true,
+  tabWidth: 2,
+  jsxBracketSameLine: false,
+  semi: true,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  requirePragma: false,
+};
